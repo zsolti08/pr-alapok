1f4f0f218378418acbfe428275face5e2b1c6327
zsolti08 <113200871+zsolti08@users.noreply.github.com>
2023-10-20 13:15:29 +0200

    Add files via upload

---
+++ Feladatok/feladat_014.py
@@ -0,0 +1,14 @@
+# feladat_014
+"""
+Kérjük be a vezeték és a keresztnevünket!
+Írassuk ki eljárás segítségével a nevünket!
+"""
+
+def nev(nev):
+
+    print( f"A nevem: {vezetek} {kereszt}." )
+
+vezetek = str(input( "Mi a vezetékneved? " ))
+kereszt = str(input( "Mi a keresztneved? " ))
+
+nev(nev)
